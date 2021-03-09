6782c4e67b482ee3a5ef99d8f5e6c28645d62e3c
Thomas M Ward <thomas@thomasward.com>
2021-03-09 16:03:38 -0500

    Add docstrings

--- pyask/ask.py
+++ pyask/ask.py
@@ -1,7 +1,10 @@
+"""Provides generic functions to ask questions on the terminal."""
+
 from functools import partial
 
 
 def print_choices(choices):
+    """Pretty prints choices on terminal with option numbers."""
     sig_figs = len(str(len(choices)))
     print("Available choices are:")
     for i, choice in enumerate(choices, 1):
@@ -9,19 +12,22 @@ def print_choices(choices):
 
 
 def prompt(question, default="", aide=""):
+    """Constructs question prompt."""
     if aide:
         return f"{question.rstrip('?')}? ({aide}) [{default}] "
     return f"{question.rstrip('?')}? [{default}] "
 
 
-def process(func, valid, x):
-    processed_x = func(x)
-    if valid(processed_x):
-        return processed_x
-    raise ValueError(f"{x} is not valid.")
+def process(func, valid, s):
+    """Processes s with func then validates it with valid."""
+    x = func(s)
+    if valid(x):
+        return x
+    raise ValueError(f"{s} is not valid.")
 
 
 def always_true(*args, **kwargs):
+    """Always returns True, no matter the arguments."""
     return True
 
 
@@ -29,6 +35,7 @@ not_empty_str = partial(process, str, lambda s: s != "")
 
 
 def ask(question, default="", aide="", process_func=not_empty_str, choices=None):
+    """Returns user answer to question once they enter a valid response."""
     if choices is not None:
         print_choices(choices)
     response = input(prompt(question, default, aide)).strip()
