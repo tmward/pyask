7afa545d7ba1dd703b1ee067f19c1d437139736d
Thomas M Ward <thomas@thomasward.com>
2021-03-11 16:58:02 -0500

    Add string regex question
    Allows programmer to ask a user for a string that must completely match
    the provided regular expression.

--- pyask/string.py
+++ pyask/string.py
@@ -1,11 +1,12 @@
 """Functions to ask questions with string answers on the terminal."""
 
 from functools import partial
+import re
 from operator import contains, eq
 
 from .ask import ask, process, always_true
 
-__all__ = ("string", "char", "yes_no")
+__all__ = ("string", "char", "yes_no", "string_regex")
 
 
 def string(question, aide="", allow_empty=False, **kwargs):
@@ -22,17 +23,24 @@ def string(question, aide="", allow_empty=False, **kwargs):
 
 
 def char(question, aide="enter a single character", allow_empty=False, **kwargs):
+    """Asks user for a single character response."""
     comparison = partial(contains, (0, 1)) if allow_empty else partial(eq, 1)
     p_func = partial(process, str, lambda s: comparison(len(s)))
     return ask(question, aide=aide, process_func=p_func, **kwargs)
 
 
 def yes_no_to_bool(s):
+    """Converts yes or y to True. false or f to False.  Case-insensitive."""
     return {"yes": True, "y": True, "no": False, "n": False}[s.casefold()]
 
 
 def yes_no(question, aide="yes or no", **kwargs):
+    """Asks user a yes or no question."""
     p_func = partial(process, yes_no_to_bool, always_true)
     return ask(question, aide=aide, process_func=p_func, **kwargs)
 
-# TODO: regex string
+def string_regex(regex, question, case_insensitive=False, **kwargs):
+    """Asks user for a string response that must completely match the regex."""
+    flags = re.IGNORECASE if case_insensitive else 0
+    p_func = partial(process, str, re.compile(regex, flags).fullmatch)
+    return ask(question, process_func=p_func, **kwargs)
